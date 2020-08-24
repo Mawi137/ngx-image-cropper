103d087b18026779daa90f73b6ed8fdd9e952b84
Akos Vandra <axos88@gmail.com>
2020-08-24 09:22:41 +0200

    export IDENTITY_IMAGE_TRANSFORM constant and services directory

--- projects/ngx-image-cropper/src/lib/interfaces/index.ts
+++ projects/ngx-image-cropper/src/lib/interfaces/index.ts
@@ -2,4 +2,4 @@ export { CropperPosition } from './cropper-position.interface';
 export { Dimensions } from './dimensions.interface';
 export { ImageCroppedEvent } from './image-cropped-event.interface';
 export { MoveStart } from './move-start.interface';
-export { ImageTransform } from './image-transform.interface';
+export { ImageTransform, IDENTITY_IMAGE_TRANSFORM } from './image-transform.interface';
--- projects/ngx-image-cropper/src/public-api.ts
+++ projects/ngx-image-cropper/src/public-api.ts
@@ -1,4 +1,5 @@
 export * from './lib/image-cropper.module';
 export * from './lib/component/image-cropper.component';
 export * from './lib/interfaces';
+export * from './lib/services';
 export { base64ToFile } from './lib/utils/blob.utils';
