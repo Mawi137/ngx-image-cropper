883b0a978db92adf28ea7a97c14ba0d54cd3a832
e <1838491745@qq.com>
2020-12-19 22:54:08 +0800

    feat: export the resizeCanvas function

--- projects/ngx-image-cropper/src/public-api.ts
+++ projects/ngx-image-cropper/src/public-api.ts
@@ -1,4 +1,5 @@
-export * from './lib/image-cropper.module';
 export * from './lib/component/image-cropper.component';
+export * from './lib/image-cropper.module';
 export * from './lib/interfaces';
 export { base64ToFile } from './lib/utils/blob.utils';
+export { resizeCanvas } from './lib/utils/resize.utils';
