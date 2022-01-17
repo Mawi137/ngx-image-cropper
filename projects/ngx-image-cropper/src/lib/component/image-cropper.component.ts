0de9fb946b81cbad793bddab9f3813227fc53906
mivanyna <mivanyna@gmail.com>
2022-01-17 14:48:19 +0200

    Add ability to move image in different directions, useful when zooming in and want to see left, right, top or bottom corner

--- projects/ngx-image-cropper/src/lib/component/image-cropper.component.ts
+++ projects/ngx-image-cropper/src/lib/component/image-cropper.component.ts
@@ -174,7 +174,8 @@ export class ImageCropperComponent implements OnChanges, OnInit {
     this.safeTransformStyle = this.sanitizer.bypassSecurityTrustStyle(
       'scaleX(' + (this.transform.scale || 1) * (this.transform.flipH ? -1 : 1) + ')' +
       'scaleY(' + (this.transform.scale || 1) * (this.transform.flipV ? -1 : 1) + ')' +
-      'rotate(' + (this.transform.rotate || 0) + 'deg)'
+      'rotate(' + (this.transform.rotate || 0) + 'deg)' +
+      'translate(' + (this.transform.translateH || 0) + '%, ' + (this.transform.translateV || 0) + '% )'
     );
   }
 
