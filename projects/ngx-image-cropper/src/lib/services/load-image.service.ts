62e0906c75ea4102aa14ad3080ebfc5043410cac
Lucas Vanhalst <lucasvanhalst@users.noreply.github.com>
2025-09-05 13:40:10 +0200

    Add 'avif' to valid image input types

--- projects/ngx-image-cropper/src/lib/services/load-image.service.ts
+++ projects/ngx-image-cropper/src/lib/services/load-image.service.ts
@@ -29,7 +29,7 @@ export class LoadImageService {
   }
 
   private isValidImageType(type: string): boolean {
-    return /image\/(png|jpg|jpeg|heic|bmp|gif|tiff|svg|webp|x-icon|vnd.microsoft.icon)/.test(type);
+    return /image\/(png|jpg|jpeg|heic|bmp|gif|tiff|svg|webp|x-icon|vnd.microsoft.icon|avif)/.test(type);
   }
 
   async loadImageFromURL(url: string, options: LoadImageOptions): Promise<LoadedImage> {
